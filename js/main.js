24f1838985970930a0cfc24002a804ce90dac62a
Marwin_Rop <68537635+Marwin5411-Winner@users.noreply.github.com>
2021-07-22 00:26:58 +0700

    m

--- js/main.js
+++ js/main.js
@@ -1,23 +1,8 @@
-//ประกาศ Let
-let tree = document.getElementById("tree");
-let grass = document.getElementById("grass");
-//
-let source = "audio/music.mp3";
-let audio = new Audio();
-audio.addEventListener("load", function() {
-  audio.play();
-}, true);
-audio.src = source;
-audio.autoplay = true;
-//Dynamic Header Scroll Event Listener
+//Toggle Btn Music Player Celia Claire
 window.addEventListener("scroll", function () {
-  let value = window.scrollY;
-  tree.style.left = -(value * 0.75) + "px";
-  grass.style.top = value * 0.5 + "px";
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
-//Toggle Btn Music Player Celia Claire
 document.getElementById("musicbtn").onclick = function musicplayer() {
   let music = document.getElementById("music");
   if (music.style.display === "none") {
